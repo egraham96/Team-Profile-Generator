0892a7a026592cc746d38622b1c4be48cc2be02b
Emma <eeg4@uw.edu>
2021-10-11 12:35:54 -0700

    Index.js Update

--- index.js
+++ index.js
@@ -1,91 +1,68 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require('lib/employee');
-const Manager = require ('lib/manager');
+const Manager = require('lib/manager');
 const Engineer = require('lib/engineer');
-const Intern = require ('lib/intern');
+const Intern = require('lib/intern');
 
-console.log('Welcome to the Team Profile Generator.',
-'According to the Professional ReadMe guide, a good ReadMe consists of the following sections:\n',
-'Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions.\n',
-'Answer the following prompts to create your own ReadMe file!');
+let officeteam = [];
+console.log('Welcome to the Team Profile Generator.\n',
+    'The Team Profile Generator is a Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person \n',
+    'Answer the following prompts to create your own HTML file!');
 
+function managerInput() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'Managername',
+        message: "What is your team manager's name?",
+    },
+    {
+        type: 'input',
+        name: 'Manageremail',
+        message: "What is your team manager's email address?",
+    },
+    {
+        type: 'input',
+        name: 'Managerid',
+        message: "What is your team manager's employee ID?",
+    },
+    {
+        type: 'input',
+        name: 'Manageroffice',
+        message: "What is your team manager's office number?",
+    },
+    ])
+        .then((data) => {
+            Managername = data.Managername;
+            Manageremail = data.Manageremail;
+            Managerid = data.Managerid;
+            Manageroffice = data.Manageroffice;
+            TeamManager = new Manager(Managername, Manageremail, Managerid, Manageroffice)
+            officeteam.push(TeamManager);
+            Nextemployee()
+        });
+}
 
-const questions = [{
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+function Nextemployee() {
+    inquirer.prompt([{
+        type: 'rawlist',
+        name: 'type',
+        message: 'Is the next employee an engineer or an intern?',
+        choices: ['Engineer', 'Intern'],
     },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?',
-    },
-    {
-      type: 'input',
-      name: 'username',
-      message: 'What is your Github username?',
-    },
-    {
-      type: 'input',
-      name: 'project',
-      message: 'What would you like the name of your project repository to be? (If project already exists, please use its GitHub name)',
-    },
-    {
-     type: 'input',
-     name: 'title',
-     message: 'Please enter a title for your project:',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please describe your project:',
-    },
-    {
-      type: 'input',
-      name: 'install',
-      message: 'Please add installation notes for your project here:',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Please describe how your project is used:',
-
-    },
-    {
-      type: 'input',
-      name: 'tech',
-      message: 'Please list any technologies used in your project:',
-    },
-    {   
-      type: 'input',
-      name: 'test',
-      message: 'Please add any project testing notes here:',
-    },
-    {
-      type: 'input',
-      name: 'contribution',
-      message: 'How can someone contribute to your project?',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Please choose a license for your project?', 
-      choices: [
-              "Apache",
-              "Berkeley",
-              "GPL",
-              "MIT",
-              "Mozilla",
-              "None",
-          ]
-      },
-    ];
-
+    ]).then((data) => {
+        employeetype = data.type;
+        if (employeetype === 'Engineer') { engineerInput() }
+        else { internInput() }
+    })
+}
 
 
 // Function used to write the generated HTML file
-    fs.writeFile('./dist/index.html', 'html', (err) =>{
-      if (err) {
+fs.writeFile('./dist/index.html', 'html', (err) => {
+    if (err) {
         throw err;
-    }})
+    }
+})
+
+managerInput();
